8d3673eb4233c85ba6108fad7e4973cf46acb152
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-08-28 14:41:59 +0200

    Adds user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { UserContext } from "./context/UserContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,24 +10,30 @@ import Manage from "./pages/Manage";
 import Create from "./pages/Create";
 import Profile from "./pages/Profile";
 import RouteNotFound from "./pages/RouteNotFound";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 export default function App() {
+  //Check if user is logged in on initial page load
+  const { load } = useLocalStorage("user");
+  const [user, setUser] = useState(load);
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="venue/:id">
-            <Route index element={<Venue />} />
-            <Route path="manage" element={<Manage />} />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="venue/:id">
+              <Route index element={<Venue />} />
+              <Route path="manage" element={<Manage />} />
+            </Route>
+            <Route path="create" element={<Create />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<RouteNotFound />} />
           </Route>
-          <Route path="create" element={<Create />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<RouteNotFound />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
