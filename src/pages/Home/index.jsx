ba8d096e5ad4b506e9ca865cca80c2be00852816
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-08-29 16:14:26 +0200

    Adds home page functionality

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+
+import VenueList from "../../components/VenueList";
+import useApi from "../../hooks/useApi";
+import Search from "./Search";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [venuesToRender, setVenuesToRender] = useState([]);
+  const { data, isLoading, isError } = useApi("/venues?limit=100");
+
+  //create a state that can be filtered and reversed to original api data
+  useEffect(() => {
+    setVenuesToRender(data);
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading venues</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Search allVenues={[...data]} setVenues={setVenuesToRender} />
+      <VenueList venues={venuesToRender} />
+    </div>
+  );
 }
