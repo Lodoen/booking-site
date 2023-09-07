cc49fbfe49ff3536bc7b79944eaf200443fda06f
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-07 14:22:54 +0200

    Adds venue page functionality

--- src/pages/Venue/index.jsx
+++ src/pages/Venue/index.jsx
@@ -1,7 +1,46 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import useApi from "../../hooks/useApi";
+import { UserContext } from "../../context/UserContext";
+import AdminFacing from "./AdminFacing";
+import CustomerFacing from "./CustomerFacing";
 
 export default function Venue() {
   let params = useParams();
-  console.log(params);
-  return <div>Individual venue page: {params.id}</div>;
+  const { user } = useContext(UserContext);
+
+  const { data, isLoading, isError } = useApi(
+    `/venues/${params.id}?_owner=true&_bookings=true`,
+  );
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading venues</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error</h1>
+      </div>
+    );
+  }
+
+  if (data.statusCode && (data.statusCode == 400 || data.statusCode == 404)) {
+    return <div>No venue with matching id</div>;
+  }
+
+  const isOwnerDefined = data && data.owner && data.owner.name;
+
+  return user &&
+    user.venueManager &&
+    isOwnerDefined &&
+    user.name === data.owner.name ? (
+    <AdminFacing venue={data} />
+  ) : (
+    <CustomerFacing venue={data} />
+  );
 }
