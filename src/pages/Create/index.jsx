fa8a8c6c738150b5aca1022a18a034d21f12ed96
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-07 15:11:38 +0200

    Adds create page functionality

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,3 +1,39 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import VenueManagement from "../../components/forms/VenueManagement";
+import useVenue from "../../hooks/useVenue";
+
 export default function Create() {
-  return <div>Create</div>;
+  const { user } = useContext(UserContext);
+  const { create } = useVenue();
+  const navigate = useNavigate();
+  const [feedback, setFeedback] = useState(undefined);
+
+  const createVenue = async (body) => {
+    try {
+      setFeedback("Loading ...");
+      const { fetchedCreate, stringifiedCreate } = await create(body);
+
+      if (fetchedCreate.ok) {
+        setFeedback("Venue created!");
+        setTimeout(() => {
+          navigate(`/venue/${stringifiedCreate.id}`);
+        }, 1500);
+      } else {
+        setFeedback(stringifiedCreate.errors[0].message);
+      }
+    } catch (error) {
+      console.log(error);
+      setFeedback("Encountered error on update");
+    }
+  };
+
+  return user && user.venueManager ? (
+    <div>
+      <VenueManagement submitFunction={createVenue} /> <p>{feedback}</p>
+    </div>
+  ) : (
+    <div>You have to be a venue manager to create venues</div>
+  );
 }
