afc812d43a3c45bc8c89e29e298d21c424265a1c
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-12 16:59:16 +0200

    Updates and styles Nav component

---
+++ src/components/Layout/Nav/index.styles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+
+  ul,
+  .dropdown-menu {
+    display: flex;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    .navigation-element {
+      display: none;
+    }
+    li:last-of-type {
+      margin-left: auto;
+    }
+  }
+
+  .desc {
+    margin: 0 10px;
+  }
+
+  .login-href {
+    padding: 10px 20px;
+  }
+
+  .login-href,
+  .dropdown-menu,
+  .navigation-element {
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    border-radius: 5px;
+  }
+
+  .dropdown-menu,
+  .navigation-element {
+    border: 0;
+    color: #fff;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    padding: 10px;
+
+    .icon {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      svg {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.screen.medium}) {
+    ul {
+      gap: 10px;
+
+      .navigation-element {
+        display: flex;
+      }
+    }
+  }
+`;
