13a4cbe18cbf4a83ad7eb20b8b95b89db880f545
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-12 16:51:20 +0200

    Updates theme

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,6 +1,11 @@
 import { ThemeProvider } from "styled-components";
 
 const theme = {
+  screen: {
+    max: "800px",
+    small: "450px",
+    medium: "600px",
+  },
   colors: {
     primary: "var(--color-primary)",
     secondary: "var(--color-secondary)",
