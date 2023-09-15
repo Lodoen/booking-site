f1842c2327c8d4b6d00583bf0c7cb0883f2561a9
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-15 13:07:12 +0200

    Updates and styles VenueManagement component

---
+++ src/components/forms/VenueManagement/VenueManagementForm/index.styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { BaseForm } from "../../BaseForm/index.styles";
+
+export const VenueManagementForm = styled(BaseForm)`
+  .input-group textarea,
+  button {
+    width: 100%;
+  }
+
+  .input-group textarea {
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 5px 10px;
+    border: 1px solid #000;
+  }
+
+  h2,
+  h3 {
+    font-weight: normal;
+  }
+
+  h2 {
+    font-size: 1.25rem;
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.screen.max}) {
+    .input-group {
+      textarea {
+        min-height: 270px;
+      }
+    }
+  }
+`;
---
+++ src/components/forms/VenueManagement/index.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const VenueShowcaseWrapper = styled.div`
+  @media screen and (min-width: ${({ theme }) => theme.screen.max}) {
+    .image-details-wrapper,
+    .details-amenities-wrapper {
+      display: grid;
+    }
+
+    .image-details-wrapper {
+      grid-template-columns: 300px 1fr;
+      gap: 50px;
+    }
+
+    .details-amenities-wrapper {
+      grid-template-columns: 1fr 1fr;
+      gap: 25px;
+    }
+  }
+`;
