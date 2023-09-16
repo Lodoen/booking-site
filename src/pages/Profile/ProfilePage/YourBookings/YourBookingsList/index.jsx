2c7dc25d38cbc7600075bdf43868bda2ce4c3fc1
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-16 14:48:42 +0200

    Updates and styles YourBookingsList component

--- src/pages/Profile/ProfilePage/YourBookings/YourBookingsList/index.jsx
+++ src/pages/Profile/ProfilePage/YourBookings/YourBookingsList/index.jsx
@@ -1,10 +1,15 @@
 import YourBookingItem from "./YourBookingItem";
+import * as S from "./index.styles";
 
 export default function YourBookingsList({ bookings }) {
   if (bookings.length > 0) {
-    return bookings.map((booking) => (
-      <YourBookingItem key={booking.id} {...booking} />
-    ));
+    return (
+      <S.YourBookingsList>
+        {bookings.map((booking) => (
+          <YourBookingItem key={booking.id} {...booking} />
+        ))}
+      </S.YourBookingsList>
+    );
   }
   return <div>You currently have no bookings.</div>;
 }
