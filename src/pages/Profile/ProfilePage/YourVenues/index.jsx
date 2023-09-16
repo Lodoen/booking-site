2e7ca96bcfe66a60cb756611805c00badac4610c
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-16 14:52:10 +0200

    Updates YourVenues component

--- src/pages/Profile/ProfilePage/YourVenues/index.jsx
+++ src/pages/Profile/ProfilePage/YourVenues/index.jsx
@@ -1,14 +1,5 @@
-import { useState } from "react";
 import VenueList from "../../../../components/VenueList";
 
-export default function YourVenues({ venues }) {
-  const [isShowingVenues, setIsShowingVenues] = useState(true);
-  return (
-    <div>
-      <button onClick={() => setIsShowingVenues(!isShowingVenues)}>
-        {isShowingVenues ? "HIDE" : "SHOW"}
-      </button>
-      {isShowingVenues && <VenueList venues={venues ? venues : []} />}
-    </div>
-  );
+export default function YourVenues({ venues, isShowingVenues }) {
+  return isShowingVenues && <VenueList venues={venues ? venues : []} />;
 }
