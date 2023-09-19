007edbcfcd77e38b644b320780fa9eac9c467a55
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-19 12:30:01 +0200

    Adds ImageCarousel component

---
+++ src/pages/Venue/VenueDetails/MediaShowcase/ImageCarousel/index.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ImageCarousel = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .media-controls,
+  figure {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .media-controls {
+    padding-top: 10px;
+    span {
+      margin: 0 10px;
+    }
+    button svg {
+      width: 25px;
+      height: 25px;
+    }
+  }
+
+  figure {
+    margin: 0;
+    img {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
+`;
