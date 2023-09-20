dfc03b7443e991ec688a5de4a56ddbe87f7c39d3
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-20 13:06:16 +0200

    Adds redirect to home on header logo

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Nav from "../Nav";
 import * as S from "./index.styles";
 import logo from "../../../assets/logo.svg";
-import { useState } from "react";
 import DropdownMenu from "./DropdownMenu";
 import { HeaderContext } from "../../../context/HeaderContext";
 
@@ -12,9 +13,11 @@ export default function Header() {
     <S.Header>
       <HeaderContext.Provider value={{ isDropdownOpen, setIsDropdownOpen }}>
         <div className="header-content">
-          <figure>
-            <img src={logo} alt="Holidaze logo" />
-          </figure>
+          <Link to="/">
+            <figure>
+              <img src={logo} alt="Holidaze logo" />
+            </figure>
+          </Link>
           <Nav />
         </div>
         {isDropdownOpen && <DropdownMenu />}
