601c1761c2e12feb4d922783b00189b8103da8ef
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-22 12:10:51 +0200

    Improves filter functionality on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,16 +2,84 @@ import { useState, useEffect } from "react";
 
 import VenueList from "../../components/VenueList";
 import useApi from "../../hooks/useApi";
-import Search from "./Search";
+import SearchAndFilter from "./SearchAndFilter";
+
+const baseFilter = {
+  search: undefined,
+  price: { min: undefined, max: undefined },
+  guests: undefined,
+  amenities: {
+    wifi: undefined,
+    parking: undefined,
+    breakfast: undefined,
+    pets: undefined,
+  },
+};
+
+function extractLocationInfo(location) {
+  const { address, city, country } = location;
+  return [address, city, country];
+}
 
 export default function Home() {
   const [venuesToRender, setVenuesToRender] = useState([]);
   const { data, isLoading, isError } = useApi("/venues?limit=100");
+  const [filter, setFilter] = useState(baseFilter);
 
-  //create a state that can be filtered and reversed to original api data
+  //Filter venuesToRender whenever filter state changes
   useEffect(() => {
-    setVenuesToRender(data);
-  }, [data]);
+    let filteredVenues = [...data];
+
+    if (filter.search) {
+      filteredVenues = filteredVenues.filter(({ name, location }) => {
+        const venueInfo = [name, ...extractLocationInfo(location)];
+        for (let i = 0; i < venueInfo.length; i++) {
+          if (
+            venueInfo[i].toLowerCase().includes(filter.search.toLowerCase())
+          ) {
+            return true;
+          }
+        }
+      });
+    }
+
+    if (filter.price.min) {
+      filteredVenues = filteredVenues.filter(
+        ({ price }) => price >= filter.price.min,
+      );
+    }
+
+    if (filter.price.max) {
+      filteredVenues = filteredVenues.filter(
+        ({ price }) => price <= filter.price.max,
+      );
+    }
+
+    if (filter.guests) {
+      filteredVenues = filteredVenues.filter(
+        ({ maxGuests }) => maxGuests >= filter.guests,
+      );
+    }
+
+    if (filter.amenities) {
+      if (filter.amenities.wifi) {
+        filteredVenues = filteredVenues.filter(({ meta }) => meta.wifi);
+      }
+
+      if (filter.amenities.parking) {
+        filteredVenues = filteredVenues.filter(({ meta }) => meta.parking);
+      }
+
+      if (filter.amenities.breakfast) {
+        filteredVenues = filteredVenues.filter(({ meta }) => meta.breakfast);
+      }
+
+      if (filter.amenities.pets) {
+        filteredVenues = filteredVenues.filter(({ meta }) => meta.pets);
+      }
+    }
+    setVenuesToRender(filteredVenues);
+  }, [data, filter]);
 
   if (isLoading) {
     return (
@@ -31,8 +99,8 @@ export default function Home() {
 
   return (
     <section>
-      <h1>Home</h1>
-      <Search allVenues={[...data]} setVenues={setVenuesToRender} />
+      <h1>List of available venues</h1>
+      <SearchAndFilter setFilter={setFilter} filter={filter} />
       <VenueList venues={venuesToRender} />
     </section>
   );
