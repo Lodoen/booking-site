97979810c3e01c189e79db8492235e4b7b98f969
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-27 16:09:56 +0200

    Updates AuthLayout Header margin

--- src/components/AuthLayout/Header/index.styles.js
+++ src/components/AuthLayout/Header/index.styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   nav figure {
-    margin: 0;
     display: flex;
     align-items: center;
     justify-content: center;
