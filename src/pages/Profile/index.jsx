9a71fda4966743717775ae88635e561d1889abed
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-27 12:27:24 +0200

    Improves user feedback on profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,20 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import ProfilePage from "./ProfilePage";
+import Feedback from "../../components/Feedback";
 
 export default function Profile() {
   const { user } = useContext(UserContext);
+
   return user && user.name ? (
     <ProfilePage user={user} />
   ) : (
-    <div>You have to logged in to view your profile</div>
+    <section>
+      <h1>Not available</h1>
+      <Feedback
+        message="You have to logged in to view your profile"
+        status="error"
+      />
+    </section>
   );
 }
