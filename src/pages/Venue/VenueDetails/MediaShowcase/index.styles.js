733a0842880101c428c0e960f35eae62bb8f7887
Lodoen <95305401+Lodoen@users.noreply.github.com>
2023-09-28 19:24:22 +0200

    Fixes z-index conflict between navbar dropdown and modal on venue page

--- src/pages/Venue/VenueDetails/MediaShowcase/index.styles.js
+++ src/pages/Venue/VenueDetails/MediaShowcase/index.styles.js
@@ -24,6 +24,7 @@ export const MediaShowcase = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    z-index: 1000;
 
     .media-wrapper {
       width: 200px;
